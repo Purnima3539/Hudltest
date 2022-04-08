5ae24db5067eb0d1c10a785e1340d1724b1e2387
Purnima3539 <53839812+Purnima3539@users.noreply.github.com>
2022-04-07 22:44:46 -0500

    new teste

--- src/test/java/com/hudl/tests/LoginTest.java
+++ src/test/java/com/hudl/tests/LoginTest.java
@@ -7,6 +7,14 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+
+/*
+I have covered few scenarios as part of this test.
+Some other scenarios on Login page are
+Functional tests: Remember Me checkbox, Need help link,Signup option, back icon to Hudl page, logging in with inactive credentials.
+Security(Non-functional) tests: Verifying password encryption, password copy/paste, login page vulnerability to SQL injection
+ */
+
 public class LoginTest extends TestBase{
     HudlLoginPage loginPage;
     HomePage homePage;
@@ -39,13 +47,19 @@ public class LoginTest extends TestBase{
     }
 
     @Test(priority=3)
-    public void InvalidLoginTest() throws  InterruptedException{
+    public void InvalidEmailTest() throws  InterruptedException{
         String email = prop.getProperty("invalidemail");
         String password = prop.getProperty("password");
         Assert.assertEquals( loginPage.getLoginErrorText(email,password), "Need help?");
     }
 
     @Test(priority=4)
+    public void InvalidPasswordTest() throws  InterruptedException{
+        String email = prop.getProperty("email");
+        String password = prop.getProperty("invalidpassword");
+        Assert.assertEquals( loginPage.getLoginErrorText(email,password), "Need help?");
+    }
+    @Test(priority=5)
     public void blankEmailPasswordTest() throws  InterruptedException {
         String email = prop.getProperty("blankemail");
         String password = prop.getProperty("blankpassword");
